a0706cbf45ea59e1e1326712c746221650895298
cphillips68k-alt <cphillips68k@gmail.com>
2026-05-03 21:52:57 -0500

    Rewrite in 68K

--- Makefile
+++ Makefile
@@ -1,17 +1,25 @@
-AS      = ca65
-LD      = ld65
+AS      = m68k-linux-gnu-as
+LD      = m68k-linux-gnu-ld
+OBJCOPY = m68k-linux-gnu-objcopy
 
-ASFLAGS = -t sim65c02
-LDFLAGS = -t sim65c02
+ASFLAGS = -m68040
+LDFLAGS = -T linker.ld
 
-qwerky.bin: qwerky.o
-	$(LD) $(LDFLAGS) -o $@ $< sim65c02.lib
+qwerky.bin: qwerky.elf
+	$(OBJCOPY) -O binary $< $@
+
+qwerky.elf: qwerky.o
+	$(LD) $(LDFLAGS) -o $@ $<
 
 qwerky.o: qwerky.s
 	$(AS) $(ASFLAGS) -o $@ $<
 
 run: qwerky.bin
-	sim65 qwerky.bin
+	qemu-system-m68k -M virt -cpu m68040 -m 16M -nographic -kernel qwerky.bin
+
+debug: qwerky.bin
+	qemu-system-m68k -M virt -cpu m68040 -m 16M -nographic -kernel qwerky.bin -s -S &
+	gdb-multiarch -ex "target remote :1234" -ex "break _start" -ex "continue"
 
 clean:
-	rm -f *.o *.bin
+	rm -f *.o *.elf *.bin
--- qwerky.s
+++ qwerky.s
@@ -1,302 +1,326 @@
-; qwerkyDOS v1.0  --  Pure 65C02 Assembly
-; For sim65 simulator
+; qwerkyDOS v1.0 - Motorola 68000
+; For QEMU: qemu-system-m68k -M virt -cpu m68040 -m 16M -nographic -kernel qwerky.bin
 
-        .setcpu "65C02"
-        .export _main
-
-; ---------------------------------------------------------------------------
-; sim65 system call vectors
-; ---------------------------------------------------------------------------
-SYS_EXIT    = $FF00
-SYS_PUTC    = $FF0F
-SYS_GETC    = $FF12
+        .section .text
+        .globl  _start
 
 ; ---------------------------------------------------------------------------
-; Memory layout
+; Exception vector table (must be first 1024 bytes)
+; QEMU virt machine loads kernel at 0x0
 ; ---------------------------------------------------------------------------
-        .segment "CODE"
+        .org    0x0000
+        .long   0x00100000              ; 0x000: Initial SSP (stack at 1MB)
+        .long   _start                  ; 0x004: Initial PC (our entry point)
+        .space  0x100-8                 ; 0x008-0x0FF: Other exception vectors (unused)
+                                        ; Bus error, address error, illegal instr, etc.
+                                        ; All point to a crash handler
 
 ; ---------------------------------------------------------------------------
-; Reset entry point
+; I/O addresses (QEMU virt machine)
 ; ---------------------------------------------------------------------------
-_main:
-reset:
-        sei
-        cld
-        ldx #$FF
-        txs
+UART_BASE   = 0xFF000000
+UART_RHR    = UART_BASE + 0   ; Receive holding register
+UART_THR    = UART_BASE + 0   ; Transmit holding register
+UART_LSR    = UART_BASE + 5   ; Line status register
+UART_LSR_RX = 0x01            ; Data ready bit
+UART_LSR_TX = 0x20            ; Transmitter empty bit
+
+; ---------------------------------------------------------------------------
+; Entry point
+; ---------------------------------------------------------------------------
+_start:
+        ; We're in supervisor mode, interrupts off
+        ; Stack already set via vector table
 
         ; Print banner
-        jsr     print_banner
-        jsr     newline
-        lda     #<str_ready
-        ldy     #>str_ready
-        jsr     print_string
-        jsr     newline
+        lea     str_banner, %a0
+        bsr     print_string
+        bsr     newline
+        lea     str_ready, %a0
+        bsr     print_string
+        bsr     newline
 
 ; ---------------------------------------------------------------------------
 ; Main command loop
 ; ---------------------------------------------------------------------------
 cmd_loop:
-        jsr     newline
-        lda     #'q'
-        jsr     putchar
-        lda     #'>'
-        jsr     putchar
-        lda     #' '
-        jsr     putchar
+        bsr     newline
+        ; Print prompt
+        move.b  #'q', %d0
+        bsr     putchar
+        move.b  #'>', %d0
+        bsr     putchar
+        move.b  #' ', %d0
+        bsr     putchar
 
-        jsr     read_line
-        jsr     newline
-        jsr     exec_command
-        jmp     cmd_loop
-
-; ---------------------------------------------------------------------------
-; Print banner
-; ---------------------------------------------------------------------------
-print_banner:
-        lda     #<str_banner
-        ldy     #>str_banner
-        jsr     print_string
-        rts
-
-str_banner:
-        .byte   $0D, $0A
-        .byte   "+------------------------------+", $0D, $0A
-        .byte   "|                              |", $0D, $0A
-        .byte   "|   qwerkyDOS v1.0             |", $0D, $0A
-        .byte   "|   (c) 1982 qwerky Micro      |", $0D, $0A
-        .byte   "|                              |", $0D, $0A
-        .byte   "|   65C02 CPU | 128K (sort of) |", $0D, $0A
-        .byte   "|                              |", $0D, $0A
-        .byte   "+------------------------------+", $0D, $0A
-        .byte   0
-
-str_ready:
-        .byte   "Ready.", 0
+        bsr     read_line
+        bsr     newline
+        bsr     exec_command
+        bra     cmd_loop
 
 ; ---------------------------------------------------------------------------
-; Command parser
-; ---------------------------------------------------------------------------
-exec_command:
-        ; Uppercase the input buffer
-        ldx     #0
-upper_loop:
-        lda     linebuf,x
-        beq     upper_done
-        cmp     #'a'
-        bcc     upper_next
-        cmp     #'z'+1
-        bcs     upper_next
-        sec
-        sbc     #$20
-        sta     linebuf,x
-upper_next:
-        inx
-        bne     upper_loop
-upper_done:
-
-        ; Empty line check
-        lda     linebuf
-        beq     cmd_return
-
-        ; Walk command table
-        lda     #<cmd_table
-        sta     $00
-        lda     #>cmd_table
-        sta     $01
-        ldy     #0
-
-cmd_match_loop:
-        lda     ($00),y
-        beq     cmd_unknown
-        tax
-        iny
-        lda     ($00),y
-        sta     $03
-        iny
-        stx     $02
-        sty     $04
-        ldx     #0
-
-cmd_strcmp:
-        lda     ($00),y
-        cmp     linebuf,x
-        bne     cmd_next
-        iny
-        inx
-        lda     ($00),y
-        bne     cmd_strcmp
-        lda     linebuf,x
-        bne     cmd_next
-        jmp     ($0002)
-
-cmd_next:
-        ldy     $04
-cmd_skip:
-        lda     ($00),y
-        beq     cmd_end_string
-        iny
-        bne     cmd_skip
-cmd_end_string:
-        iny
-        lda     ($00),y
-        bne     cmd_match_loop
-
-cmd_unknown:
-        lda     #<str_unknown
-        ldy     #>str_unknown
-        jsr     print_string
-cmd_return:
-        rts
-
-str_unknown:
-        .byte   "? Unknown command. Type HELP.", $0D, $0A, 0
-
-; ---------------------------------------------------------------------------
-; Command handlers
-; ---------------------------------------------------------------------------
-cmd_help:
-        lda     #<str_help
-        ldy     #>str_help
-        jsr     print_string
-        rts
-
-cmd_cls:
-        ldx     #25
-cls_loop:
-        jsr     newline
-        dex
-        bne     cls_loop
-        rts
-
-cmd_beep:
-        lda     #$07
-        jsr     putchar
-        rts
-
-cmd_mem:
-        lda     #<str_mem
-        ldy     #>str_mem
-        jsr     print_string
-        rts
-
-cmd_ver:
-        lda     #<str_ver
-        ldy     #>str_ver
-        jsr     print_string
-        rts
-
-cmd_quit:
-        jmp     $FF00
-
-; ---------------------------------------------------------------------------
-; Command table: handler address (2 bytes) + null-terminated name
-; ---------------------------------------------------------------------------
-cmd_table:
-        .word   cmd_help
-        .byte   "HELP",0
-        .word   cmd_cls
-        .byte   "CLS",0
-        .word   cmd_beep
-        .byte   "BEEP",0
-        .word   cmd_mem
-        .byte   "MEM",0
-        .word   cmd_ver
-        .byte   "VER",0
-        .word   cmd_quit
-        .byte   "Q",0
-        .word   cmd_quit
-        .byte   "QUIT",0
-        .byte   0
-
-str_help:
-        .byte   "Commands: HELP, CLS, BEEP, MEM, VER, Q", $0D, $0A, 0
-str_mem:
-        .byte   "Free: lots. Top of RAM: $C000 (ROM starts).", $0D, $0A, 0
-str_ver:
-        .byte   "qwerkyDOS v1.0   (c) 1982 qwerky Micro", $0D, $0A, 0
-
-; ---------------------------------------------------------------------------
-; Console I/O using sim65 monitor
-; ---------------------------------------------------------------------------
-putchar:
-        jmp     $FF0F
-
-getchar:
-        jmp     $FF12
-
-; ---------------------------------------------------------------------------
-; Print null-terminated string, pointer in A/Y (low/high)
+; Print string (null-terminated) pointed to by A0
 ; ---------------------------------------------------------------------------
 print_string:
-        sta     $10
-        sty     $11
-        ldy     #0
+        movem.l %d0/%a0, -(%sp)
 ps_loop:
-        lda     ($10),y
+        move.b  (%a0)+, %d0
         beq     ps_done
-        jsr     putchar
-        iny
-        bne     ps_loop
+        bsr     putchar
+        bra     ps_loop
 ps_done:
+        movem.l (%sp)+, %d0/%a0
         rts
 
 ; ---------------------------------------------------------------------------
 ; Print newline (CR+LF)
 ; ---------------------------------------------------------------------------
 newline:
-        lda     #$0D
-        jsr     putchar
-        lda     #$0A
-        jsr     putchar
+        move.b  #0x0D, %d0
+        bsr     putchar
+        move.b  #0x0A, %d0
+        bsr     putchar
         rts
 
 ; ---------------------------------------------------------------------------
-; Read a line into linebuf with backspace handling
+; Output character in D0.B to UART
+; ---------------------------------------------------------------------------
+putchar:
+        move.l  %d1, -(%sp)
+        move.l  #UART_LSR, %a0
+putchar_wait:
+        move.b  (%a0), %d1
+        andi.b  #UART_LSR_TX, %d1
+        beq     putchar_wait
+        move.l  #UART_THR, %a0
+        move.b  %d0, (%a0)
+        move.l  (%sp)+, %d1
+        rts
+
+; ---------------------------------------------------------------------------
+; Input character from UART, returns in D0.B
+; ---------------------------------------------------------------------------
+getchar:
+        move.l  #UART_LSR, %a0
+getchar_wait:
+        move.b  (%a0), %d0
+        andi.b  #UART_LSR_RX, %d0
+        beq     getchar_wait
+        move.l  #UART_RHR, %a0
+        move.b  (%a0), %d0
+        rts
+
+; ---------------------------------------------------------------------------
+; Read a line into linebuf, handle backspace
 ; ---------------------------------------------------------------------------
 read_line:
-        ldx     #0
-rl_key_loop:
-        jsr     getchar
-        cmp     #$0D
-        beq     rl_done
-        cmp     #$0A
-        beq     rl_done
-        cmp     #$08
-        beq     rl_backspace
-        cmp     #$7F
-        beq     rl_backspace
-        cmp     #$20
-        bcc     rl_key_loop
-        cmp     #$7F
-        bcs     rl_key_loop
-        cpx     #79
-        bcs     rl_key_loop
-        sta     linebuf,x
-        inx
-        jsr     putchar
-        bne     rl_key_loop
+        movem.l %d0/%d1/%a0, -(%sp)
+        lea     linebuf, %a0
+        moveq   #0, %d1                 ; character count
 
-rl_backspace:
-        cpx     #0
-        beq     rl_key_loop
-        dex
-        lda     #$08
-        jsr     putchar
-        lda     #$20
-        jsr     putchar
-        lda     #$08
-        jsr     putchar
-        bne     rl_key_loop
+rl_loop:
+        bsr     getchar
+        ; Check for Enter
+        cmpi.b  #0x0D, %d0
+        beq     rl_done
+        cmpi.b  #0x0A, %d0
+        beq     rl_done
+        ; Check for backspace
+        cmpi.b  #0x08, %d0
+        beq     rl_back
+        cmpi.b  #0x7F, %d0
+        beq     rl_back
+        ; Printable?
+        cmpi.b  #0x20, %d0
+        blo     rl_loop
+        cmpi.b  #0x7F, %d0
+        bhs     rl_loop
+        ; Buffer full? (79 chars max)
+        cmpi.w  #79, %d1
+        bhs     rl_loop
+        ; Store and echo
+        move.b  %d0, (%a0)+
+        addq.w  #1, %d1
+        bsr     putchar
+        bra     rl_loop
+
+rl_back:
+        tst.w   %d1
+        beq     rl_loop
+        subq.w  #1, %d1
+        subq.l  #1, %a0
+        ; Echo backspace-space-backspace
+        move.b  #0x08, %d0
+        bsr     putchar
+        move.b  #0x20, %d0
+        bsr     putchar
+        move.b  #0x08, %d0
+        bsr     putchar
+        bra     rl_loop
 
 rl_done:
-        lda     #0
-        sta     linebuf,x
+        move.b  #0, (%a0)
+        movem.l (%sp)+, %d0/%d1/%a0
         rts
 
 ; ---------------------------------------------------------------------------
-; BSS data
+; Command execution
 ; ---------------------------------------------------------------------------
-        .segment "BSS"
-linebuf:    .res    80
+exec_command:
+        movem.l %d0/%d1/%a0/%a1, -(%sp)
+        lea     linebuf, %a0
+
+        ; Uppercase the line
+        move.l  %a0, %a1
+up_loop:
+        move.b  (%a1), %d0
+        beq     up_done
+        cmpi.b  #'a', %d0
+        blo     up_next
+        cmpi.b  #'z'+1, %d0
+        bhs     up_next
+        subi.b  #0x20, %d0
+        move.b  %d0, (%a1)
+up_next:
+        addq.l  #1, %a1
+        bra     up_loop
+up_done:
+
+        ; Empty line?
+        tst.b   (%a0)
+        beq     cmd_exit
+
+        ; Walk command table
+        lea     cmd_table, %a1
+
+cmd_scan:
+        ; Check end of table (null name)
+        move.w  (%a1), %d0
+        beq     cmd_unknown
+        ; %a1 points to handler address (4 bytes) + name string
+        ; Compare strings
+        move.l  %a1, %a2                ; save entry start
+        addq.l  #4, %a1                 ; skip handler address
+        move.l  %a0, %a3                ; linebuf pointer
+cmd_cmp:
+        move.b  (%a1)+, %d0
+        beq     cmd_check_end           ; end of table name
+        cmp.b   (%a3)+, %d0
+        beq     cmd_cmp
+        bra     cmd_next_entry
+
+cmd_check_end:
+        tst.b   (%a3)
+        beq     cmd_match               ; both ended = match
+
+cmd_next_entry:
+        ; Skip remaining name bytes
+cmd_skip:
+        tst.b   (%a1)+
+        bne     cmd_skip
+        ; %a1 now points to next entry
+        move.l  %a1, %a2                ; check if next entry exists
+        move.w  (%a2), %d0
+        bne     cmd_scan
+        bra     cmd_unknown
+
+cmd_match:
+        move.l  %a2, %a1
+        move.l  (%a1), %a0              ; load handler address
+        jsr     (%a0)
+        bra     cmd_exit
+
+cmd_unknown:
+        lea     str_unknown, %a0
+        bsr     print_string
+
+cmd_exit:
+        movem.l (%sp)+, %d0/%d1/%a0/%a1
+        rts
+
+; ---------------------------------------------------------------------------
+; Command handlers
+; ---------------------------------------------------------------------------
+cmd_help:
+        lea     str_help, %a0
+        bsr     print_string
+        rts
+
+cmd_cls:
+        moveq   #25, %d0
+cls_loop:
+        bsr     newline
+        subq.b  #1, %d0
+        bne     cls_loop
+        rts
+
+cmd_beep:
+        move.b  #0x07, %d0
+        bsr     putchar
+        rts
+
+cmd_mem:
+        lea     str_mem, %a0
+        bsr     print_string
+        rts
+
+cmd_ver:
+        lea     str_ver, %a0
+        bsr     print_string
+        rts
+
+cmd_quit:
+        ; Halt the CPU
+        stop    #0x2700
+        rts
+
+; ---------------------------------------------------------------------------
+; Data
+; ---------------------------------------------------------------------------
+        .section .data
+
+str_banner:
+        .asciz  "\r\n+------------------------------+\r\n|                              |\r\n|   qwerkyDOS v1.0             |\r\n|   (c) 1982 qwerky Micro      |\r\n|                              |\r\n|   68000 CPU | 16MB RAM (virt)|\r\n|                              |\r\n+------------------------------+\r\n"
+
+str_ready:
+        .asciz  "Ready."
+
+str_unknown:
+        .asciz  "? Unknown command. Type HELP.\r\n"
+
+str_help:
+        .asciz  "Commands: HELP, CLS, BEEP, MEM, VER, Q\r\n"
+
+str_mem:
+        .asciz  "Free: about 16MB. It's 2024, not 1982.\r\n"
+
+str_ver:
+        .asciz  "qwerkyDOS v1.0   (c) 1982 qwerky Micro\r\n"
+
+; ---------------------------------------------------------------------------
+; Command table: 4-byte handler address + null-terminated name
+; ---------------------------------------------------------------------------
+        .section .rodata
+cmd_table:
+        .long   cmd_help
+        .asciz  "HELP"
+        .long   cmd_cls
+        .asciz  "CLS"
+        .long   cmd_beep
+        .asciz  "BEEP"
+        .long   cmd_mem
+        .asciz  "MEM"
+        .long   cmd_ver
+        .asciz  "VER"
+        .long   cmd_quit
+        .asciz  "Q"
+        .long   cmd_quit
+        .asciz  "QUIT"
+        .long   0                       ; end marker
+
+; ---------------------------------------------------------------------------
+; BSS (uninitialized data)
+; ---------------------------------------------------------------------------
+        .section .bss
+linebuf:
+        .space  80
